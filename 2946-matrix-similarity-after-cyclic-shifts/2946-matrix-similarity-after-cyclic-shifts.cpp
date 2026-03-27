f3888e674caf38eaac9409603b74c9e7eb1202c4
Tanmay Tiwari <158596526+TanmayTiwarii@users.noreply.github.com>
2026-03-27 18:06:49 +0530

    Time: 2 ms (34.16%), Space: 31.1 MB (91.3%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> temp(mat.size(),vector<int>(mat[0].size()));
         for(int i=0;i<mat.size();i++){
             for(int j=0;j<mat[0].size();j++){
-                temp[i][j]=mat[i][(j+k)%mat[0].size()];
-            }
-        }
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                if(mat[i][j]!=temp[i][j]){
+                if(mat[i][(j+k)%mat[0].size()]!=mat[i][j]){
                     return false;
                 }
             }
