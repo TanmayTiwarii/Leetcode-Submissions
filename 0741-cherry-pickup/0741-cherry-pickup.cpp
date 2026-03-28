f36b31677236c1f1e21a546e125398cd95ae7be4
Tanmay Tiwari <158596526+TanmayTiwarii@users.noreply.github.com>
2026-03-28 23:11:11 +0530

    Time: 18 ms (97%), Space: 13 MB (98.24%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.cpp
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    int func(vector<vector<int>>& grid,int r1,int c1,int r2,int c2,vector<vector<vector<vector<int>>>> &dp){
-        int n = grid.size();
-        if(r1>=n || c1>=n || r2>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1){
+     int dp[51][51][51];
+    int func(int r1 , int c1 , int r2 , int n , vector<vector<int>> &grid )
+    {
+        int c2=r1+c1-r2;
+        
+        if(r1>=n || r2>=n || c1>=n || c2>=n || grid[r1][c1]==-1 || grid[r2][c2]==-1)
+        {
             return INT_MIN;
         }
-        if(dp[r1][c1][r2][c2]!=-1){
-            return dp[r1][c1][r2][c2];
+        
+        if(dp[r1][c1][r2]!=-1)
+        {
+            return dp[r1][c1][r2];
         }
-        if(r1==n-1 && c1==n-1){
+        
+        if(r1==n-1 && c1==n-1)
+        {
             return grid[r1][c1];
         }
-        int ans = 0;
-        ans+=grid[r1][c1];
-        if(r1!=r2){
-            ans+=grid[r2][c2];
+        
+        int ans=grid[r1][c1];
+        
+        if(r1!=r2)
+        {
+            ans += grid[r2][c2];
         }
-        int a = func(grid,r1+1,c1,r2+1,c2,dp);
-        int b = func(grid,r1+1,c1,r2,c2+1,dp);
-        int c = func(grid,r1,c1+1,r2+1,c2,dp);
-        int d = func(grid,r1,c1+1,r2,c2+1,dp);
-        return  dp[r1][c1][r2][c2] =  ans + max({a, b, c, d});
+        
+        int temp= max(func(r1+1 , c1 , r2+1 , n , grid) , func(r1 , c1+1 , r2 , n , grid));
+        temp = max(temp , func(r1+1 , c1 , r2 , n, grid));
+        temp = max(temp , func(r1 , c1+1 , r2+1 , n , grid));
+        
+        ans+=temp;
+        
+        return dp[r1][c1][r2]=ans;
     }
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        vector<vector<vector<vector<int>>>> dp(
-            n,
-            vector<vector<vector<int>>>(
-                n,
-                vector<vector<int>>(
-                    n,
-                    vector<int>(n, -1)
-                )
-            )
-        );
-        return max(func(grid,0,0,0,0,dp),0);
+        memset(dp , -1 , sizeof(dp));
+        int n=grid.size();
+        return max(0 , func(0,0,0,n , grid));
     }
 };
