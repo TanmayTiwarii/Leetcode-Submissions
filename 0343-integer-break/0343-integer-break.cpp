0c10080abd650e96453e40229cce7d4e41a054f3
Tanmay Tiwari <158596526+TanmayTiwarii@users.noreply.github.com>
2026-03-29 19:07:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.65%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int func(int n,vector<int> &dp){
-        if(n==0){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            ans=max(ans,i*(func(n-i,dp)));
-        }
-        return dp[n] = ans;
-    }
-
     int integerBreak(int n) {
         int ans=0;
-        vector<int> dp(n+1,-1);
-        for(int i=1;i<n;i++){
-            ans=max(ans,i*(func(n-i,dp)));
+        vector<int> dp(n+1,1);
+        for(int len = 1;len<=n;len++){
+            for(int i=1;i<=len;i++){
+                if(len==n && i==n){
+                    continue;
+                }
+                dp[len] = max(dp[len], i*dp[len-i]);
+            }
         }
-        return ans;
+        return dp[n];
     }
 };
