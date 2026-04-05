dfa55c3f0276e0ef78a200ee308898c7238f5188
Tanmay Tiwari <158596526+TanmayTiwarii@users.noreply.github.com>
2026-04-05 23:28:06 +0530

    Time: 111 ms (21.31%), Space: 81.4 MB (45.49%) - LeetHub

---
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int destroyTargets(vector<int>& nums, int space) {
+        map<int,int> count;
+        for(auto it:nums){
+            count[it%space]++;
+
+        }
+        int ans=INT_MAX,temp=0;
+        for(auto it:nums){
+            if(count[it%space]>=temp){
+                if(count[it%space]==temp)
+                ans=min(ans,it);
+                else
+                ans=it;
+                temp=count[it%space];
+            }
+        }
+        return ans;
+        
+    }
+};
